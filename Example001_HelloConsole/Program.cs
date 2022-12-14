57618377c7c72071f29f80bba0232143752b7990
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2022-12-14 20:44:56 +0700

    "added exsample002"

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("2 Hello, World!");
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Writec(username);
